9f0f41eeda07bf0391e06723ce88a0d4a8d2bc90
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-03-05 12:11:13 +0100

    made menu dynamic, added some additional logic for this and added context provider to avoid prop drilling

--- website_menu/src/App.js
+++ website_menu/src/App.js
@@ -3,40 +3,45 @@ import NavBar from "./components/NavBar";
 import Button from "./components/Button";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import "./index.css";
+import MenuContext from "./context/MenuContext";
+import LinkObject from "./utils/LinkObject";
+
+import { useState } from "react";
 
 function App() {
+  const links = [
+    LinkObject("Home", "/home.png"),
+    LinkObject("Sevices", "/cloud-service.png"),
+    LinkObject("Pricing", "/price-tag.png"),
+    LinkObject("Blog", "/communication.png"),
+  ];
+
+  const [isLoggedIn, setLogIn] = useState(false);
+
+  const onClick = (event) => {
+    event.preventDefault();
+    setLogIn(!isLoggedIn);
+  };
+
+  const context = {
+    links: links,
+    isLoggedIn: isLoggedIn,
+    onClick: onClick,
+  };
+
   return (
     <div>
-      <NavBar>
-          <Button className={"btn border border-dark m-1 p-1 b"} 
-            value={"Home"} 
-            icon={<img src="/home.png"></img>}
-          />
-          <Button 
-            className={"btn border m-1 border border-dark b"} 
-            value={"Services"} 
-            icon={<img src="/cloud-service.png"></img>}
-            />
-          <Button 
-            className={"btn border m-1 border border-dark b"} 
-            value={"Pricing"} 
-            icon={<img src="/price-tag.png"></img>}
-          />
-          <Button 
-            className={"btn border m-1 border border-dark b"} 
-            value={"Blog"} 
-            icon={<img src="/communication.png"></img>}
-          />
-      </NavBar>
-      <hr className="m-0" />
-      <Main value={"Welcome to this website"} />
-      <hr />
-      <Footer>
+      <MenuContext.Provider value={context}>
+        <NavBar />
+        <hr className="m-0" />
+        <Main value={"Welcome to this website"} />
+        <hr />
+        <Footer>
           <Button href={"/"} value={"About"} />
           <Button href={"/"} value={"Contact"} />
           <Button href={"/"} value={"React Quickly 2E"} />
-      </Footer>
+        </Footer>
+      </MenuContext.Provider>
     </div>
   );
 }
--- website_menu/src/components/Main.js
+++ website_menu/src/components/Main.js
@@ -1,10 +1,22 @@
+import { useContext } from "react";
 
-function Main({ value }){
-    return (
-        <main className="m-3">
-            <h1>{value}</h1>
-        </main>
-    );
-};   
+import MenuContext from "../context/MenuContext";
+import Button  from "./Button";
+
+function Main({ value }) {
+    const { isLoggedIn, onClick } = useContext(MenuContext);
+    const buttonStyle = `btn ${isLoggedIn ? "btn-danger" : "btn-success"}`;
+
+  return (
+    <main className="m-3">
+      <h1>{value}</h1>
+      <Button
+        value={isLoggedIn ? "Log out" : "Log in"}
+        className={buttonStyle}
+        onClick={onClick}
+        />
+    </main>
+  );
+}
 
 export default Main;
